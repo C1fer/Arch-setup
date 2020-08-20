265005f6720496768a2004125838bab09afd90c1
C1fer <41005657+C1fer@users.noreply.github.com>
2020-08-20 15:36:35 -0400

    Update mesa.sh

--- mesa.sh
+++ mesa.sh
@@ -5,7 +5,8 @@ else
  echo "test"
 fi
 #Mesa 64bit
-mkdir /tmp/mesa && cd /tmp/mesa wget https://aur.archlinux.org/cgit/aur.git/snapshot/mesa-git.tar.gz 
+mkdir /tmp/mesa && cd /tmp/mesa 
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/mesa-git.tar.gz 
 tar -xf mesa-git.tar.gz 
 if grep -q "MESA_WHICH_LLVM=" /tmp/mesa/mesa-git/PKGBUILD; then
  sed -i 's/MESA_WHICH_LLVM=4/MESA_WHICH_LLVM=3/' "/tmp/mesa/mesa-git/PKGBUILD"
@@ -25,4 +26,3 @@ else
  
 fi 
 rm -rf /tmp/mesa
-

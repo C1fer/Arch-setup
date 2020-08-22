0906597bb39893aac13143df8932ba23c4234e2a
C1fer <41005657+C1fer@users.noreply.github.com>
2020-08-22 09:03:06 -0400

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -18,6 +18,7 @@ sudo pacman-key --populate archlinux
 sudo pacman-key --keyserver hkp://ipv4.pool.sks-keyservers.net:11371 -r 3056513887B78AEB 8A9E14A07010F7E3
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman-key --lsign-key 8A9E14A07010F7E3
+sudo pacman -Sc --noconfirm
 
 #Parallel Compilation
 if grep -q "MAKEFLAGS=" /etc/makepkg.conf; then
@@ -32,6 +33,13 @@ sudo pacman -Syyu --noconfirm pacman-contrib yay zsh-theme-powerlevel10k-git p7z
 sudo pacman -Rdd --noconfirm llvm-libs mesa
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' |tee -a ~/.zshrc && echo 'export PATH="/usr/lib/ccache/bin/:$PATH"' | tee -a ~/.zshrc
 yay -S --noconfirm conan noto-fonts-cjk ttf-opensans ttf-meslo-nerd-font-powerlevel10k gstreamer gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plugin-libde265 mesa-git lib32-mesa-git mesa-demos libva-utils vulkan-tools llvm-git llvm-libs-git lib32-llvm-git clang-git meson 
+
+if grep -q "(invalid or corrupted package (PGP signature))" ~/debug; then
+ echo "script failed. Exiting..."
+ exit
+else  
+ :
+fi
 
 #Others
 yay -S --noconfirm plasma-meta adapta-kde-git adapta-gtk-theme mkvtoolnix-gui plasma5-applets-eventcalendar spek-git spectacle-git brave qbittorrent mangohud goverlay-git puddletag-qt5-git wine-tkg-staging-fsync-vkd3d-opt-git winetricks-git psensor corectrl-git ark gwenview krename steam gnome-disk-utility nano qdirstat-git grub-customizer htop gedit galculator gparted audacious systemd-swap schedtool kde-servicemenus-rootactions cronie modprobed-db qmplay2-git ffmpegthumbs  neofetch papirus-folders-git 

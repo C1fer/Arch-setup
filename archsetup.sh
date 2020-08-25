c1974baa54cce65e85db911140006b7fb5d6165d
C1fer <41005657+C1fer@users.noreply.github.com>
2020-08-25 08:52:06 -0400

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -31,20 +31,12 @@ fi
 
 #SOFTWARE
 sudo pacman -Syyu --noconfirm pacman-contrib yay zsh-theme-powerlevel10k-git p7zip unrar  fuseiso git base-devel ninja cmake sdl2 qt5 python2 python-pip boost catch2 fmt libzip lz4 mbedtls nlohmann-json openssl opus zlib ccache zstd ntfs-3g ufw gufw wget nano  bluez bluez-utils
-sudo pacman -Rdd --noconfirm llvm-libs mesa
+sudo pacman -Rdd --noconfirm llvm-libs lib32-llvm-libs mesa 
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
 export PATH="/usr/lib/ccache/bin/:$PATH"
 export EDITOR="/usr/bin/nano"' | tee -a ~/.zshrc
 
-yay -S --noconfirm conan noto-fonts-cjk ttf-opensans ttf-meslo-nerd-font-powerlevel10k gstreamer gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plugin-libde265 mesa-git lib32-mesa-git mesa-demos libva-utils vulkan-tools llvm-git llvm-libs-git lib32-llvm-git clang-git meson 
-
-if grep -q "(invalid or corrupted package (PGP signature))" /home/rusbel/debug; then
- echo "script failed. Exiting..."
- exit
-else  
- :
-fi
-
+yay -S --noconfirm conan noto-fonts-cjk ttf-opensans ttf-meslo-nerd-font-powerlevel10k gstreamer gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plugin-libde265 mesa-demos libva-utils vulkan-tools llvm-git llvm-libs-git lib32-llvm-git clang-git  mesa-git lib32-mesa-git meson 
 #Others
 yay -S --noconfirm plasma-meta adapta-kde-git adapta-gtk-theme mkvtoolnix-gui plasma5-applets-eventcalendar spek-git spectacle brave qbittorrent mangohud goverlay-git puddletag-qt5-git wine-tkg-staging-fsync-vkd3d-opt-git winetricks-git psensor corectrl-git ark gwenview krename steam gnome-disk-utility nano qdirstat-git grub-customizer htop gedit galculator gparted audacious systemd-swap schedtool kde-servicemenus-rootactions cronie modprobed-db qmplay2-git ffmpegthumbs  neofetch papirus-folders-git 
 
@@ -64,7 +56,7 @@ enable_bt
 
 #modprobe & cron
 modprobed-db && modprobed-db store 
-crontab -e 0 */1* * *   /usr/bin/modprobed-db store &> /dev/null
+#crontab -e 0 */1* * *   /usr/bin/modprobed-db store &> /dev/null
 
 #Papirus custom folder color
 papirus-folders -C bluegrey --theme Papirus-Dark

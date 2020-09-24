233c8e97db3bdeedd0651703b120b1effe41a205
C1fer <41005657+C1fer@users.noreply.github.com>
2020-09-24 19:10:55 -0400

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -45,7 +45,7 @@ yay -S --noconfirm plasma-meta arc-kde-git arc-gtk-theme mkvtoolnix-gui plasma5-
 #Cleaning
 sudo pacman -Rdd --noconfirm vim chromium discover oxygen plasma-thunderbolt plasma-browser-integration
 yay -Sc --noconfirm && sudo pacman -Rns --noconfirm $(pacman -Qtdq)
-sudo rm -rf /var/cache/pacman/pkg ~/.cache/yay
+#sudo rm -rf /var/cache/pacman/pkg ~/.cache/yay
 #pacman_conf_signon
 pacman_conf
 

851e155cc73cd45ca320a7f2800e29f7a9d4f435
C1fer <41005657+C1fer@users.noreply.github.com>
2021-02-17 09:26:23 -0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -98,37 +98,7 @@ Include = /etc/pacman.d/mirrorlist
 # tips on creating your own repositories.
 #[custom]
 #SigLevel = Optional TrustAll
-#Server = file:///home/custompkgs
-
-[chaotic-aur]
-# Brazil
-## By: PedroHLC and UFSCar (Dead right now)
-#Server = https://lonewolf.pedrohlc.com/\$repo/\$arch
-#Server = http://lonewolf-builder.duckdns.org/\$repo/\$arch
-
-# USA
-## By: GarudaLinux and Fosshost
-Server = https://builds.garudalinux.org/repos/\$repo/\$arch
-## By: LordKitsuna
-Server = https://repo.kitsuna.net/\$arch
-
-# Netherlands
-## By: Var Bhat and LiteServer
-Server = https://chaotic.tn.dedyn.io/\$arch
-
-# Burgos, Spain
-## By: JKANetwork
-Server = https://repo.jkanetwork.com/repo/\$repo/\$arch
-
-# Germany
-## By: Nico
-Server = https://chaotic.dr460nf1r3.me/repos/\$repo/\$arch
-## By: ParanoidBangL
-Server = http://chaotic.bangl.de/\$repo/\$arch
-
-# Seoul, Korea
-## By: Ryoichi <t.me/r377yx>
-Server = https://mirror.maakpain.kro.kr/garuda/\$repo/\$arch" | sudo tee /etc/pacman.conf >&-
+#Server = file:///home/custompkgs | sudo tee /etc/pacman.conf >&-
 
 }
 
@@ -305,35 +275,6 @@ Include = /etc/pacman.d/mirrorlist
 # tips on creating your own repositories.
 #[custom]
 #SigLevel = Optional TrustAll
-#Server = file:///home/custompkgs
-
-[chaotic-aur]
-# Brazil
-## By: PedroHLC and UFSCar (Dead right now)
-#Server = https://lonewolf.pedrohlc.com/\$repo/\$arch
-#Server = http://lonewolf-builder.duckdns.org/\$repo/\$arch
-
-# USA
-## By: GarudaLinux and Fosshost
-Server = https://builds.garudalinux.org/repos/\$repo/\$arch
-## By: LordKitsuna
-Server = https://repo.kitsuna.net/\$arch
-
-# Netherlands
-## By: Var Bhat and LiteServer
-Server = https://chaotic.tn.dedyn.io/\$arch
-
-# Burgos, Spain
-## By: JKANetwork
-Server = https://repo.jkanetwork.com/repo/\$repo/\$arch
-
-# Germany
-## By: Nico
-Server = https://chaotic.dr460nf1r3.me/repos/\$repo/\$arch
-## By: ParanoidBangL
-Server = http://chaotic.bangl.de/\$repo/\$arch
-
-# Seoul, Korea
-## By: Ryoichi <t.me/r377yx>
-Server = https://mirror.maakpain.kro.kr/garuda/\$repo/\$arch" | sudo tee /etc/pacman.conf >&-
+#Server = file:///home/custompkgs 
+| sudo tee /etc/pacman.conf >&-
 }

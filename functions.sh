7a6d9575ba8dacf2b5e99815404088e3f9ce37c0
C1fer <41005657+C1fer@users.noreply.github.com>
2021-06-05 08:56:54 -0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -269,5 +269,5 @@ chaotic () {
 sudo pacman-key --init
 sudo pacman-key --recv-key 3056513887B78AEB
 sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-'{keyring,mirrorlist}'.pkg.tar.zst'
+sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-'{keyring,mirrorlist}'.pkg.tar.zst'
 }
